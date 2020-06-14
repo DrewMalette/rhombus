f1abc5b292604d418cc8bba99a7afdcee5520f56
Drew M <urbanbonfire@protonmail.com>
2020-06-14 00:43:12 -0500

    Scene code is more readable

--- framework/core/scene.py
+++ framework/core/scene.py
@@ -1,15 +1,13 @@
-# media/input, mechanics, file handling
+#
 
-import os
-import xml.etree.ElementTree as ET
-
-from .mob import Mob
-from .tileset import Tileset
+#from .mob import Mob
+#from .tileset import Tileset
 from . import filepaths
+from . import utilities
 
 class Scene:
 
-	def __init__(self, uid, game, map_filename):
+	def __init__(self, uid, game, filename):
 
 		self.uid = uid
 		self.game = game
@@ -21,94 +19,14 @@ class Scene:
 		self.furniture = {}
 		self.loot = {}
 		
-		#self.uid = filename
-		
-		tree = ET.parse(map_filename)
-		root = tree.getroot()
-		
-		self.cols = int(root.attrib["width"])
-		self.rows = int(root.attrib["height"])
-		
-		self.tilewidth = int(root.attrib["tilewidth"])
-		self.tileheight = int(root.attrib["tileheight"])
-		self.tilesize = self.tilewidth # assumes a square tile
-		
-		self.tileset = Tileset(self.tilewidth, self.tileheight)
-		
+		#self.uid = map_filename		
+			
 		self.loot = {}
 		self.loot_count = 0
-		
-		self.layerdata = { "bottom": None,
-				           "middle": None,
-					  	   "top": None,
-					  	   "collide": None
-						 }
-
 		self.switches = {}
+		self.layerdata = { "bottom": None, "middle": None, "top": None, "collide": None }		
 		
-		for tilesettag in root.iter("tileset"):
-			filename = tilesettag.attrib["source"]
-			tilestree = ET.parse(os.path.join(filepaths.scene_path, filename))
-			tilesroot = tilestree.getroot()
-			for tileset in tilesroot.iter("tileset"):
-				for i in tileset.iter("image"):
-					filename = i.attrib["source"]
-					firstgid = tilesettag.attrib["firstgid"]
-					self.tileset.update(filename, firstgid) # ummmmmmm....?
-					
-		for layer in root.iter("layer"):
-			for data in layer.iter("data"):
-				name = layer.attrib['name']
-				rawdata = data.text.split(",")
-				cleandata = []
-				for tile in rawdata:
-					cleandata.append(tile.strip())
-				self.layerdata[name] = cleandata
-				
-		for layer in root.iter("objectgroup"):
-			for rect in layer.iter("object"):
-				rectattribs = {}
-				for v in rect.attrib.keys():
-					rectattribs[v] = rect.attrib[v]
-				for proptag in rect.iter("properties"):
-					for propchild in proptag.iter("property"):
-						index = propchild.attrib["name"]
-						value = propchild.attrib["value"]
-						rectattribs[index] = value
-				
-				uid = rectattribs["id"]
-				col = int(float(rectattribs["x"]) / self.tilewidth)
-				row = int(float(rectattribs["y"]) / self.tileheight)
-				if rectattribs["type"] == "player":
-					if self.game.player is None:
-						print("player object is not defined")
-						print("exiting")
-						pygame.quit()
-						exit()
-					self.live_mobs["player"] = self.game.player
-					self.live_mobs["player"].scene = self
-					self.live_mobs["player"].place(col, row)
-				elif rectattribs["type"] == "switch":
-					x = int(float(rectattribs["x"]) / self.tilewidth) * self.tilewidth
-					y = int(float(rectattribs["y"]) / self.tileheight) * self.tileheight
-					facing = rectattribs["facing"]
-					try:
-						c = int(rectattribs["col"])
-						r = int(rectattribs["row"])
-						self.switches[uid] = [pygame.Rect((x,y,self.tilewidth,self.tileheight)), rectattribs["Filename"], (c,r), facing]
-					except:
-						#print("defaulting to map defined placement position")
-						self.switches[uid] = [pygame.Rect((x,y,self.tilewidth,self.tileheight)), rectattribs["Filename"], None, facing]
-				elif rectattribs["type"] == "mob":
-					self.live_mobs[uid] = mob.Mob("content/image/" + rectattribs["Filename"], rectattribs["name"])
-					self.live_mobs[uid].scene = self
-					utilities.place(self.live_mobs[uid], col, row, self)
-				#elif rectattribs["type"] == "static":
-				#	filepath = "content/image/" + rectattribs["Filename"]
-				#	name = rectattribs["name"]
-				#	self.sprites[uid] = sprite.Static(filepath, name)
-				#	self.sprites[uid].scene = self
-				#	self.sprites[uid].place(col,row)
+		utilities.load_tmx(filename, self)
 
 	def add_loot(self, filename, x, y):
 	
--- framework/core/utilities.py
+++ framework/core/utilities.py
@@ -1,5 +1,12 @@
 
+import os
+import xml.etree.ElementTree as ET
+
 import pygame
+
+from .tileset import Tileset
+from . import mob
+from . import filepaths
 
 def load_image(filename, colourkey=None):
 
@@ -52,3 +59,81 @@ def load_tileset(filename, width, height, firstgid=1):
 			gid += 1
 	
 	return textures
+	
+def load_tmx(filename, scene_obj):
+
+	tree = ET.parse(filename)
+	root = tree.getroot()
+	
+	scene_obj.cols = int(root.attrib["width"])
+	scene_obj.rows = int(root.attrib["height"])
+	
+	scene_obj.tilewidth = int(root.attrib["tilewidth"])
+	scene_obj.tileheight = int(root.attrib["tileheight"])
+	scene_obj.tilesize = scene_obj.tilewidth # assumes a square tile
+	
+	scene_obj.tileset = Tileset(scene_obj.tilewidth, scene_obj.tileheight)
+	
+	for tilesettag in root.iter("tileset"):
+		filename = tilesettag.attrib["source"]
+		tilestree = ET.parse(os.path.join(filepaths.scene_path, filename))
+		tilesroot = tilestree.getroot()
+		for tileset in tilesroot.iter("tileset"):
+			for i in tileset.iter("image"):
+				filename = i.attrib["source"]
+				firstgid = tilesettag.attrib["firstgid"]
+				scene_obj.tileset.update(filename, firstgid) # ummmmmmm....?
+				
+	for layer in root.iter("layer"):
+		for data in layer.iter("data"):
+			name = layer.attrib['name']
+			rawdata = data.text.split(",")
+			cleandata = []
+			for tile in rawdata:
+				cleandata.append(tile.strip())
+			scene_obj.layerdata[name] = cleandata
+			
+	for layer in root.iter("objectgroup"):
+		for rect in layer.iter("object"):
+			rectattribs = {}
+			for v in rect.attrib.keys():
+				rectattribs[v] = rect.attrib[v]
+			for proptag in rect.iter("properties"):
+				for propchild in proptag.iter("property"):
+					index = propchild.attrib["name"]
+					value = propchild.attrib["value"]
+					rectattribs[index] = value
+			
+			uid = rectattribs["id"]
+			col = int(float(rectattribs["x"]) / scene_obj.tilewidth)
+			row = int(float(rectattribs["y"]) / scene_obj.tileheight)
+			if rectattribs["type"] == "player":
+				if scene_obj.game.player is None:
+					print("player object is not defined")
+					print("exiting")
+					pygame.quit()
+					exit()
+				scene_obj.live_mobs["player"] = scene_obj.game.player
+				scene_obj.live_mobs["player"].scene = scene_obj
+				scene_obj.live_mobs["player"].place(col, row)
+			elif rectattribs["type"] == "switch":
+				x = int(float(rectattribs["x"]) / scene_obj.tilewidth) * scene_obj.tilewidth
+				y = int(float(rectattribs["y"]) / scene_obj.tileheight) * scene_obj.tileheight
+				facing = rectattribs["facing"]
+				try:
+					c = int(rectattribs["col"])
+					r = int(rectattribs["row"])
+					scene_obj.switches[uid] = [pygame.Rect((x,y,scene_obj.tilewidth,scene_obj.tileheight)), rectattribs["Filename"], (c,r), facing]
+				except:
+					#print("defaulting to map defined placement position")
+					scene_obj.switches[uid] = [pygame.Rect((x,y,scene_obj.tilewidth,scene_obj.tileheight)), rectattribs["Filename"], None, facing]
+			elif rectattribs["type"] == "mob":
+				scene_obj.live_mobs[uid] = mob.Mob("content/image/" + rectattribs["Filename"], rectattribs["name"])
+				scene_obj.live_mobs[uid].scene_obj = scene_obj
+				utilities.place(scene_obj.live_mobs[uid], col, row, scene_obj)
+			#elif rectattribs["type"] == "static":
+			#	filepath = "content/image/" + rectattribs["Filename"]
+			#	name = rectattribs["name"]
+			#	scene.sprites[uid] = sprite.Static(filepath, name)
+			#	scene.sprites[uid].scene = scene
+			#	scene.sprites[uid].place(col,row)
