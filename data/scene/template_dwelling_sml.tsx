fa01662f6e32aaa28d3d7e75655dde4673ebe116
Drew M <drewmalette@protonmail.com>
2020-12-08 14:25:59 -0600

    content update

--- data/scene/template_dwelling_sml.tsx
+++ data/scene/template_dwelling_sml.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="template_dwelling_sml" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="template_dwelling_sml.png" trans="ff00ff" width="80" height="64"/>
+<tileset version="1.4" tiledversion="1.4.2" name="template_dwelling_sml" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="template_dwelling_sml.png" trans="ff00ff" width="160" height="128"/>
 </tileset>
