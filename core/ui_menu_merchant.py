73da79af13fbe58aa95fadf562b76567c4040bec
Drew M <drewmalette@protonmail.com>
2021-01-31 18:29:37 -0600

    multi-pane ui paradigm prototype

---
+++ core/ui_menu_merchant.py
@@ -0,0 +1,37 @@
+
+# software spec:
+# 3 panes
+# Selector Dialogue, Buy/Sell
+# Main pane; works similar to inventory (child pane changes based on Selector position)
+# Label for currency (player.money = { "gold": 0, "silver": 0, "copper": 0 })
+# Child pane displays what goods are for sale ("Buy") or what goods you have for sale ("Sell").
+#   'item name' $price
+#   as well, not all buyers buy all products. a potion seller won't buy swords, etc.,
+#
+# Game.ui_settings
+# UI_Group (a collection of UI elements)
+#
+#
+#
+
+class Game:
+    def __init__(self):
+        self.ui_settings = None
+
+class UI_Theme: # Game.ui_settings
+    def __init__(self, game, data):
+        self.game = game
+        self.font = data["ui-font"]
+
+class UI_Group:
+    def __init__(self, game):
+        self.game = game
+        self.settings = self.game.ui_settings
+
+class UI_Merchant(UI_Group):
+    def __init__(self, game):
+        UI_Group.__init__(self, game)
+        
+        self.selector = UI_Selector(self.game)
+        self.currency_display = UI_CurrencyDisplay(self.game)
+        self.item_display = UI_ItemDisplay(self.game)
